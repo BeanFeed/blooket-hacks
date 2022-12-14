5767ed449569fe19ea68d38073c5f13647e426a4
BeanFeed <86930720+BeanFeed@users.noreply.github.com>
2022-12-14 09:52:55 -0500

    Update gui.js

--- gui.js
+++ gui.js
@@ -654,7 +654,7 @@ details summary ~ * {
         e.code == 'KeyE' && (GUI.hidden = !GUI.hidden)
     };
     function toggleAuto(e) {
-        e.code == 'KeyA' && (autoAnswer = !autoAnswer)   
+        e.code == 'ShiftLeft' && (autoAnswer = !autoAnswer)   
     }    
     addEventListener('keypress', toggleHidden);
     addEventListener('keypress', toggleAuto);
